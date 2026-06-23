a03e9138f2e93b7a7fccd1b9fa02f5362df20927
Your Name <thabdoul0512@gmail.com>
2026-06-23 13:10:09 +0200

    import landind.ts

--- Frontend/Clando/src/app/features/landing/landing.ts
+++ Frontend/Clando/src/app/features/landing/landing.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router'; // 
 
 @Component({
     selector: 'app-landing',
